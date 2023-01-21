fab39cb1a9edce473935c2a2ba3ea441bc37000b
Aan <dalang@petruknisme.com>
2023-01-22 02:49:50 +0700

    Initial support for ESXI

---
+++ ESXI/outputs.tf
@@ -0,0 +1,39 @@
+output "DC01_interfaces" {
+  value = esxi_guest.DC01.network_interfaces
+}
+
+output "DC01_ips" {
+  value = esxi_guest.DC01.ip_address
+}
+
+output "DC02_interfaces" {
+  value = esxi_guest.DC02.network_interfaces
+}
+
+output "DC02_ips" {
+  value = esxi_guest.DC02.ip_address
+}
+
+output "DC03_interfaces" {
+  value = esxi_guest.DC03.network_interfaces
+}
+
+output "DC03_ips" {
+  value = esxi_guest.DC03.ip_address
+}
+
+output "SRV02_interfaces" {
+  value = esxi_guest.SRV02.network_interfaces
+}
+
+output "SRV02_ips" {
+  value = esxi_guest.SRV02.ip_address
+}
+
+output "SRV03_interfaces" {
+  value = esxi_guest.SRV03.network_interfaces
+}
+
+output "SRV03_ips" {
+  value = esxi_guest.SRV03.ip_address
+}
